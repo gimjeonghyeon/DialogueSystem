044ab16566609acba1340245b93450e68e485e8e
jeonghyeon <wmjh0925@naver.com>
2021-10-19 11:39:11 +0900

    스터디 피드백 주석 추가

--- Assets/Scripts/DialogueSystemWindow.cs
+++ Assets/Scripts/DialogueSystemWindow.cs
@@ -31,6 +31,8 @@ public class DialogueSystemWindow : MonoBehaviour
     {
         string tempText = "";
 
+        // TODO : 대화가 진행되는 부분을 트윈을 사용하여 할 수 있는지 -> 두트윈 프로 텍스트 매쉬프로에 글자수 출력하는게 있음, 두트윈에서 변수값 임의로 조절할 수 있는게 있음, 두트윈 텍스트매쉬프로 글자출력 찾아보기
+        // TODO : 커맨드를 삽입, 사운드(액션), 딜레이, 속도 조절 -> 엑셀로 관리 (json 은 한눈에 관리가 어려움) 엑셀로 프로젝트에 넣고 Json으로 export한 뒤에 클래스형태로 대화 출력되게 (오토로 대화 넘어가는지 여부)
         for (int i = 0; i < dialogue.Length; i++)
         {
             tempText += dialogue[i];
